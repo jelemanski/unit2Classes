f318156776e26712a6cfe9996fd607967c3dbb45
jelemanski <jelemanski@NN121-HA-14.HS.sd203.org>
2015-09-23 10:30:07 -0500

    target

---
+++ Chapter 3/Target.java
@@ -0,0 +1,33 @@
+import java.awt.Graphics; 
+import java.awt.Graphics2D; 
+import javax.swing.JComponent; 
+
+/**
+ * An example of a method - replace this comment with your own
+ *  that describes the operation of the method
+ *
+ * @pre     preconditions for the method
+ *          (what the method assumes about the method's parameters and class's state)
+ * @post    postconditions for the method
+ *          (what the method guarantees upon completion)
+ * @param   y   description of parameter y
+ * @return  description of the return value
+ */
+
+
+/**
+ * Write a description of class Target here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Target extends JComponent 
+{
+   public void Paint(Graphics g) 
+   {
+
+       Graphics2D = (Graphics2D) g;
+       Target Target1 = new Target(100,100);
+    }
+}
+       
